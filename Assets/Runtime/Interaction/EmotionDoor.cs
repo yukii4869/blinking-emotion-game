63710398d30d2135afdec5959c46e8f9828efa73
yukii4869 <107083531+yukii4869@users.noreply.github.com>
2026-04-20 02:31:06 +0200

    added door open surprised

--- Assets/Runtime/Interaction/EmotionDoor.cs
+++ Assets/Runtime/Interaction/EmotionDoor.cs
@@ -2,18 +2,18 @@ using UnityEngine;
 public class EmotionDoor : MonoBehaviour, IInteractable
 {
     [SerializeField] private FaceInputManager face;
+    [SerializeField] private EmotionHoldCondition condition;
     [SerializeField] private Animator doorAnim;
+
 
     public void Interact()
     {
-         Debug.Log("Door Interaction!");
-        if (face.currentEmotion == Emotion.Surprised)
+        if (condition.TaskCompleted)
         {
-            Debug.Log("Door open");
-            doorAnim.SetTrigger("Open");
-            //OpenDoor();
+            return;
         }
-        else
-            Debug.Log("Door requires surprise!");
+        condition.ActivateCondition();
+
+
     }
 }
