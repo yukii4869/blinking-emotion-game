bb9a94ce7d52fa444645f932fa5006f2449b1d81
yukii4869 <107083531+yukii4869@users.noreply.github.com>
2026-06-26 02:54:51 +0200

    added roommanager

--- Assets/Runtime/CSharp/GameLoop/RoomManager.cs
+++ Assets/Runtime/CSharp/GameLoop/RoomManager.cs
@@ -5,6 +5,11 @@ using TMPro;
 public class RoomManager : MonoBehaviour
 {
     public static RoomManager instance;
+    public Transform mainHallRoot;
+    public Transform leftWingRoot;
+    public Transform rightWingRoot;
+
+
 
     public List<Room> rooms = new List<Room>();
     public List<Room> mainHall = new List<Room>();
@@ -21,42 +26,65 @@ public class RoomManager : MonoBehaviour
 
     void Start()
     {
-        // Rooms in der Reihenfolge der Hierarchie laden
-        rooms = new List<Room>(GetComponentsInChildren<Room>());
+        mainHall = new List<Room>(mainHallRoot.GetComponentsInChildren<Room>());
+        leftWing = new List<Room>(leftWingRoot.GetComponentsInChildren<Room>());
+        rightWing = new List<Room>(rightWingRoot.GetComponentsInChildren<Room>());
 
         AssignRoomNumbers();
         AssignDeliverySpots();
         UpdateRoomLabels();
     }
-
     private void AssignRoomNumbers()
     {
-        
-        for (int i = 0; i < rooms.Count; i++)
-        {
-            rooms[i].roomNumber = startNumber + i;
-        }
+        int main = startNumber;        // z.B. 200
+        int left = startNumber + 8;   // z.B. 210
+        int right = startNumber + 24;  // z.B. 220
+
+        foreach (var r in mainHall)
+            r.roomNumber = main++;
+
+        foreach (var r in leftWing)
+            r.roomNumber = left++;
+
+        foreach (var r in rightWing)
+            r.roomNumber = right++;
     }
 
     private void AssignDeliverySpots()
     {
-        foreach (var room in rooms)
-        {
-            DeliverySpot spot = room.GetComponentInChildren<DeliverySpot>();
-            if (spot != null)
-            {
-                spot.room = room;
-            }
-        }
+        foreach (var room in mainHall)
+            LinkSpot(room);
+
+        foreach (var room in leftWing)
+            LinkSpot(room);
+
+        foreach (var room in rightWing)
+            LinkSpot(room);
+    }
+
+    private void LinkSpot(Room room)
+    {
+        DeliverySpot spot = room.GetComponentInChildren<DeliverySpot>();
+        if (spot != null)
+            spot.room = room;
     }
 
     private void UpdateRoomLabels()
     {
-        foreach (var room in rooms)
-        {
-            if (room.roomLabel != null)
-                room.roomLabel.text = room.roomNumber.ToString();
-        }
+        foreach (var room in mainHall)
+            UpdateLabel(room);
+
+        foreach (var room in leftWing)
+            UpdateLabel(room);
+
+        foreach (var room in rightWing)
+            UpdateLabel(room);
+    }
+
+    private void UpdateLabel(Room room)
+    {
+        if (room.roomLabel != null)
+            room.roomLabel.text = room.roomNumber.ToString();
     }
 
     public Room GetFreeRoom()
